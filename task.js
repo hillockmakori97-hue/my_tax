a2e39d595bb4e37692e0cc5b8097ba13b59283b7
hillockmakori97-hue <hillockmakori97@gmail.com>
2026-07-02 22:28:34 +0300

    strath cont

--- task.js
+++ task.js
@@ -1,7 +1,7 @@
 /*TASK 1: Using Python or PHP or Java or Ruby or JavaScript
 Write a program that prompts the user to enter the base and height of a triangle and returns its area.*/
-function add_numbers(x,y){
-    z=x+y
+function add_numbers(x, y) {
+    z = x + y
     console.log(z)
 }
 
@@ -37,61 +37,92 @@ Continue with the same program and calculate an individual’s Net Salary using:
 
 Write a normal program but use functions if you feel comfortable.*/
 
-function nhif (basic_salary,benefits){
-    gross_salary=basic_salary+benefits
-    if (gross_salary<6000){ 
-        nhif=150
-    }else if (gross_salary>6000 && gross_salary<8000){
-        nhif=300
+document.getElementById('tax_form').addEventListener('submit', function (event) {
+    event.preventDefault()
 
-    }else if (gross_salary>=8000 && gross_salary<12000){
-        nhif=400
-    }else if (gross_salary>=120000 && gross_salary<15000){
-        nhif = 500 
-    }else if (gross_salary>=15000 && gross_salary< 20000){
-        nhif = 600 
-    }else if (gross_salary>=20000 && gross_salary < 25000){
-        nhif = 700 
-    }else if ( gross_salary>=25000 && gross_salary < 30000){
-        nhif = 800
-    }else if ( gross_salary>=30000 && gross_salary<35000){
-        nhif = 900
-    }else if ( gross_salary >= 350000 && gross_salary <40000){
-        nhif = 1000
-    }else if ( gross_salary>=40000 && gross_salary<45000){
-        nhif = 1100
-    }else if ( gross_salary>=45000 && gross_salary<50000){
-        nhif = 1200
-    }else if ( gross_salary>=50000 && gross_salary<60000){
-        nhif = 1300
-    }else if ( gross_salary>=60000 && gross_salary<70000){
-        nhif = 1400
-    }else if ( gross_salary>=700000 && gross_salary<80000){
-        nhif = 1500
-    }else if ( gross_salary>=80000 && gross_salary<90000){
-        nhif = 1600
-    }else if ( gross_salary>=100000){
-        nhif = 1700
+    let basic_salary = Number(document.getElementById('basic_salary').value)
+    let benefits = Number(document.getElementById('benefits').value)
+    function calculate_basic_salary(basic_salary, benefits) {
+        return basic_salary + benefits
     }
-    return nhif
+    let gross_salary = calculate_basic_salary(basic_salary, benefits)
+    document.getElementById('gross').innerHTML = gross_salary
 
-}
 
-function nssf(gross_salary){
-    if (gross_salary>18000){
-        nssf=0.16*gross_salary
-        res= nssf
+    function nhif_calc(gross_salary) {
+        let nhif
+        if (gross_salary < 6000) {
+            nhif = 150
+        } else if (gross_salary > 6000 && gross_salary < 8000) {
+            nhif = 300
+
+        } else if (gross_salary >= 8000 && gross_salary < 12000) {
+            nhif = 400
+        } else if (gross_salary >= 120000 && gross_salary < 15000) {
+            nhif = 500
+        } else if (gross_salary >= 15000 && gross_salary < 20000) {
+            nhif = 600
+        } else if (gross_salary >= 20000 && gross_salary < 25000) {
+            nhif = 700
+        } else if (gross_salary >= 25000 && gross_salary < 30000) {
+            nhif = 800
+        } else if (gross_salary >= 30000 && gross_salary < 35000) {
+            nhif = 900
+        } else if (gross_salary >= 350000 && gross_salary < 40000) {
+            nhif = 1000
+        } else if (gross_salary >= 40000 && gross_salary < 45000) {
+            nhif = 1100
+        } else if (gross_salary >= 45000 && gross_salary < 50000) {
+            nhif = 1200
+        } else if (gross_salary >= 50000 && gross_salary < 60000) {
+            nhif = 1300
+        } else if (gross_salary >= 60000 && gross_salary < 70000) {
+            nhif = 1400
+        } else if (gross_salary >= 700000 && gross_salary < 80000) {
+            nhif = 1500
+        } else if (gross_salary >= 80000 && gross_salary < 90000) {
+            nhif = 1600
+        } else if (gross_salary >= 100000) {
+            nhif = 1700
+        }
+        return nhif
+
     }
-    
-    else {
-        res='Inelligile for Nssf '
-    } 
 
-}
+    function nssf_calc(gross_salary) {
+        let nssf
+        if (gross_salary > 18000) {
+            nssf = 0.16 * gross_salary
+        }
+
+        else {
+            nssf = 0
+        }
+        return nssf
+
+    }
 
 
-function NSDF(gross_salary){
-    NSDF=gross_salary*0.015
-    return nsdf
-}
-// function taxable_income()
+    function nhdf_calc(gross_salary) {
+        let nhdf
+        nhdf = gross_salary * 0.015
+        return nhdf
+    }
+    let nhdf = nhdf_calc(gross_salary)
+    document.getElementById('nhdf').innerHTML = nhdf.toFixed(2)
+
+    let nhif = nhif_calc(gross_salary)
+    document.getElementById('nhif').innerHTML = nhif.toFixed(2)
+
+    let nssf = nssf_calc(gross_salary)
+    document.getElementById('nssf').innerHTML = nssf.toFixed(2)
+
+
+    function taxable_income_calc(gross_salary, nhif, nhdf, nssf) {
+        let taxable_income
+        taxable_income = (gross_salary) - (nhif, nhdf, nssf)
+        return taxable_income
+    }
+    let taxable_income=taxable_income_calc(gross_salary,nhif,nhdf,nssf)
+    document.getElementById('taxableincome').innerHTML=taxable_income.toFixed(2)
+})
